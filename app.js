9579111c22180150fdb8401698862188cfd682a6
Wajahat <wajahat10947@gmail.com>
2023-10-24 17:39:56 -0700

    assignment

---
+++ app.js
@@ -0,0 +1,320 @@
+// ********************************ALEART**********************************
+
+// 1.	Write a script to greet your website visitor using JS alert box.
+
+// alert('Welcome To Java Script Alert');
+
+
+// ------------------------------------------------------------------------------------
+
+
+// // 2.	Write a script to display following message on your web page:
+
+// alert('Error! Please Enter a Valid Password.');
+
+
+
+// -------------------------------------------------------------------------------------
+
+
+
+
+// // 3.	Write a script to display following message on your web page: (Hint : Use line break)
+
+// alert('Welcome To JS Land... \nHappy Coding ');
+
+
+
+
+// -------------------------------------------------------------------------------------
+
+
+
+
+// // 4.	Write a script to display following messages in sequence:
+
+// alert('Welcome To JS Land.');
+// alert('Happy Coding ');
+
+
+
+
+// -------------------------------------------------------------------------------------
+
+
+
+
+
+// // 5.	Generate the following message through browser’s developer console:
+
+// alert("Hello!.. I can run JS through my Web Browser's console");
+
+
+
+
+
+
+
+
+// ******************************VARIABLES   FOR	STRINGS********************************//
+
+
+
+
+
+
+// 1. Declare a variable called username
+
+// let username = "Wajahat";
+// console.log(username);
+
+
+
+
+// -------------------------------------------------------------------------------------
+
+
+
+
+
+
+// // 2. Declare a variable called myName & assign to it a string that represents your Full Name.
+
+// let myName = "Muhammad Wajahat";
+// console.log(myName);
+
+
+
+
+
+// -------------------------------------------------------------------------------------
+
+
+
+
+
+
+// // 3. Write a script to
+// // a. declare a JS variable, titled message
+// // b. assign “Hello World” to the variable message
+// // c. display the message in alert box 
+
+// let JS = "Hello JS";
+// alert(JS);
+
+
+
+
+
+
+// ------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+// // 4. Write a script to save student’s bio data in JS variables and show the data in alert boxes.
+
+// let studentName = "Wajahat";
+// let age = "25";
+
+// alert(studentName + " " + age);
+// alert(studentName);
+// alert(age);
+
+
+
+
+
+
+// ------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+// // 5. Write a script to display the following alert using one JS variable:
+
+// let pizza = " PIZZA \n PIZZ \n PIZ \n PI \n P";
+// alert(pizza);
+
+
+
+
+
+// ------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+// // 6. Fortune Teller: Why pay a fortuneteller when you can just program your fortune yourself?
+// // Store the following into variables: number of children, partner’s name, geographic location, and job title.
+
+// let children = '2';
+// let partner = 'ABC';
+// let glocation = 'Pakistan';
+// let job = 'Application Developer';
+
+// alert("You'll be a "+ job + " in " +  glocation + ", " + " and Married to " + partner + " with " + children + " childrens");
+
+
+
+
+
+
+// ------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+// // 7. Declare a variable called email & assign to it a string that represents your Email Address (e.g. abc@xyz.com). Show
+// // the below mentioned message in an alert box. (Hint: use string concatenation)
+
+// let email = "wajahat10947@gmail.com";
+
+// alert("My Email Address is " + email);
+
+
+
+
+
+
+// ------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+// // 8. Declare a variable called book & give it the value “A smarter way to learn JavaScript”. Display the following
+// // message in an alert box:
+
+// let book = "'A smarter way to learn JavaScript'"
+
+// alert("I'm Trying to Learn from The Book " + book);
+
+
+
+
+
+
+
+// ------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+// 9. Use the variables, declared in exercise 4 & show the details giving meaning to data. Like “My name is Ali Khan”, “I am
+// 17 years old”, “I study in SSUET”, “I take Mobile & Cloud Computing, Module A classes”
+
+
+// let name = "Muhammad Wajahat";
+// let age = "25";
+// let study = "Expertizo";
+// let classes = "Module A Classes";
+
+// alert("My Name is " + name + ", I'm " + age + " Years Old " + ", I Study in " + study + ", I take Mobile & Cloud Computing, " + classes);
+
+
+
+
+
+// ------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+// 10. Write a script to display this in browser through JS
+
+// document.writeln("Yay! I Can write content through JavaScript")
+
+
+
+
+
+
+
+
+
+
+// ***********************************VARIABLES	FOR	NUMBERS***********************************
+
+
+// 1. Declare a variable called age & assign to it your age. Show your age in an alert box.
+
+// let age = 25;
+// alert("I'm " + age + " Years Old");
+
+
+
+
+
+// // ------------------------------------------------------------------------------------------------------------------
+
+
+
+
+// // 2. Declare & initialize a variable to keep track of how many times a visitor has visited a web page. Show his/her
+// // number of visits on your web page. For example: “You have visited this site N times”.
+
+
+// let visit = 12;
+// alert("You have visited this site " + visit + " Times");
+
+
+
+
+
+
+// // ------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+// // 3. Declare a variable called birthYear & assign to it your birth year. Show the following message in your browser:
+
+
+// let birthYear = 1998;
+// document.write("My Birth Year is " + birthYear);
+
+
+
+
+
+// // 3. Declare a variable called birthYear & assign to it your birth year. Show the following message in your browser:
+
+
+
+
+
+// // 4. A visitor visits an online clothing store www.xyzClothing.com .
+// //  Write a script to store in variables the following information:
+// // a. Visitor’s name
+// // b. Product title
+// // c. Quantity i.e. how many products a visitor wants to order
+// // Show the following message in your browser: “John Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+
+
+
+// let visitorName = "Jhon Doe";
+// let productTitle = "T-Shirts";
+// let order = 5;
+
+// document.write(visitorName + " ordered " + order +" "+ productTitle + " on ABC Clothing Store")
